308aee280a726d7cd0fd8a2c49a111ff06de88c7
Mike Chang <mchang95@gmail.com>
2021-09-07 09:37:32 -0700

    updated docstrings

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/api/CAISO", methods=["POST"])
+@app.route('/api/CAISO', methods=['POST'])
 def get_zip_file(): 
     """ Gets zip file from CAISO API. """
 
-    caiso_request = CaisoRequest.create_new_request(request.json["data"])
+    caiso_request = CaisoRequest.create_new_request(request.json['data'])
     caiso_request.get_data()
     caiso_request.stream_file()
     response = caiso_request.extract_and_parse()
--- models.py
+++ models.py
@@ -18,7 +18,7 @@ class CaisoRequest():
 
         self.url = url
         self.report_type = queryname
-        self.response = ''
+        self.resp = ''
         self.file = ''
         self.filename = ''
 
@@ -28,7 +28,7 @@ class CaisoRequest():
         """ Creates a new request. """
         
         url = CaisoRequest.url_constructor(form_data)
-        return CaisoRequest(url, form_data["queryname"])
+        return CaisoRequest(url, form_data['queryname'])
 
 
     @staticmethod
@@ -37,30 +37,35 @@ class CaisoRequest():
             queryname, constructs url for CAISO request.
 
         Accepts: {
-            queryname: "ENE_SLRS",
-            tac_zone_name: "TAC_PGE",
-            schedule: "Export",
-            market_run_id: "RTM",
-            startdatetime: "20210818T07:00-0000"
-            enddatetime: "20210819T07:00-0000"
+            queryname: 'ENE_SLRS',
+            tac_zone_name: 'TAC_PGE',
+            schedule: 'Export',
+            market_run_id: 'RTM',
+            startdatetime: '20210818T07:00-0000'
+            enddatetime: '20210819T07:00-0000'
             } 
             
-        Returns: f"{BASE_URL}?queryname=ENE_SLR&market_run_id=RTM..." 
+        Returns: f'{BASE_URL}?queryname=ENE_SLR&market_run_id=RTM...' 
         """
 
+        # check for basic required fields
         required_fields = ['startdatetime', 'enddatetime', 'queryname']
-
         if not all(field in list(form_data.keys()) for field in required_fields): 
-            raise KeyError("Missing start date, end date, or query name")
+            raise KeyError('Missing start date, end date, or query name')
 
-        queries = [f"{key}={form_data[key]}" for key in form_data]
+        # if PRC_LMP and RTM, then replace queryname with appropriate for request
+        if (form_data['queryname'] == 'PRC_LMP' 
+            and form_data['market_run_id'] == 'RTM'): 
+            form_data['queryname'] = 'PRC_INTVL_LMP'
 
-        if (form_data['queryname'] == "ENE_TRANS_LOSS"): 
+        queries = [f'{key}={form_data[key]}' for key in form_data]
+
+        if (form_data['queryname'] == 'ENE_TRANS_LOSS'): 
             queries.append(f'version=9')
         else: 
-            queries.append(f"version={OASIS_VERSION}")
+            queries.append(f'version={OASIS_VERSION}')
 
-        query_string = "&".join(queries)
+        query_string = '&'.join(queries)
 
         return BASE_URL + query_string
 
@@ -72,7 +77,7 @@ class CaisoRequest():
             resp = requests.get(self.url)
             self.resp = resp
         except: 
-            raise ValueError("Invalid URL received.")
+            raise ValueError('Invalid URL received.')
 
 
     def stream_file(self): 
@@ -80,25 +85,25 @@ class CaisoRequest():
 
         self.file = zipfile.ZipFile(io.BytesIO(self.resp.content))
         self.filename = self.file.namelist()[0]
-        if (self.filename == "INVALID_REQUEST.xml"): 
-            raise ValueError("Invalid response from CAISO API. Please check request and try again.")
+        if (self.filename == 'INVALID_REQUEST.xml'): 
+            raise ValueError('Invalid response from CAISO API. Please check request and try again.')
     
 
     def extract_and_parse(self): 
         """ Calls appropriate function to parse based on self.report_type. """
         response = {}
 
-        response["header"] = self._construct_header()
+        response['header'] = self._construct_header()
 
         # calls parser depending on report type
-        if (self.report_type == "ENE_SLRS"): 
-            response["reports"] = self._parse_ENE_SLRS()
+        if (self.report_type == 'ENE_SLRS'): 
+            response['reports'] = self._parse_ENE_SLRS()
         
-        if (self.report_type == "PRC_LMP" or self.report_type == "PRC_INTVL_LMP"): 
-            response["reports"] = self._parse_PRC_LMP()
+        if (self.report_type == 'PRC_LMP' or self.report_type == 'PRC_INTVL_LMP'): 
+            response['reports'] = self._parse_PRC_LMP()
 
-        if (self.report_type == "ENE_TRANS_LOSS"): 
-            response["reports"] = self._parse_ENE_TRANS_LOSS()
+        if (self.report_type == 'ENE_TRANS_LOSS'): 
+            response['reports'] = self._parse_ENE_TRANS_LOSS()
 
         response['reports'] = self.sort_reports_by_interval_start(response['reports'])
         return response
@@ -107,8 +112,8 @@ class CaisoRequest():
     def _get_xmlns(self, tag): 
         """ Gets and returns xmlns from tag as string. """
 
-        tuple = tag.partition("}")
-        uri = tuple[0] + "}"
+        tuple = tag.partition('}')
+        uri = tuple[0] + '}'
         return uri
 
 
@@ -116,9 +121,9 @@ class CaisoRequest():
         """ Parses file to construct response header.
             Returns: 
                 header: {
-                    report: "ENE_SLRS",
-                    mkt_type: "RTM",
-                    UOM: "MW"
+                    report: 'ENE_SLRS',
+                    mkt_type: 'RTM',
+                    UOM: 'MW'
                     update_interval: 300000,
                 },
         """
@@ -132,14 +137,17 @@ class CaisoRequest():
 
             # parses and collects metadata into dict entry 'header'
             for metadata in root.iter(f'{uri}REPORT_HEADER'):
-                header["REPORT"] = metadata.find(f'{uri}REPORT').text
+                header['REPORT'] = metadata.find(f'{uri}REPORT').text
                 header['MKT_TYPE'] = metadata.find(f'{uri}MKT_TYPE').text
 
                 # some reports (e.g. trans_loss) do not include a UOM
-                if (metadata.find(f'{uri}UOM')): 
-                    header['UOM'] = metadata.find(f'{uri}UOM').text
+                uom = metadata.find(f'{uri}UOM')
+                if (uom != None): 
+                    header['UOM'] = uom.text
 
-            header["update_interval"] = CaisoRequest.intervals[header['MKT_TYPE']]
+                break
+
+            header['update_interval'] = CaisoRequest.intervals[header['MKT_TYPE']]
 
             return header
     
@@ -173,6 +181,7 @@ class CaisoRequest():
             'TOT_EXP_MW': 'Export',
             'TOT_GEN_MW': 'Generation',
             'TOT_IMP_MW': 'Import',
+            'TOT_LOAD_MW': 'Load',
         }
 
         with self.file.open(self.filename) as my_file:
@@ -188,8 +197,8 @@ class CaisoRequest():
                 resource_name = entry.find(f'{uri}RESOURCE_NAME').text
 
                 # if caiso_totals is not to be included, skip any iteration with
-                # resource name of "caiso_totals"
-                if not include_totals and resource_name == "Caiso_Totals": 
+                # resource name of 'caiso_totals'
+                if not include_totals and resource_name == 'Caiso_Totals': 
                     continue
 
                 if (resource_name not in reports): 
@@ -203,9 +212,9 @@ class CaisoRequest():
 
                 
                 report_data = {}
-                report_data["interval_start_gmt"] = entry.find(f'{uri}INTERVAL_START_GMT').text
-                report_data["interval"] = entry.find(f'{uri}INTERVAL_NUM').text
-                report_data["value"] = entry.find(f'{uri}VALUE').text
+                report_data['interval_start_gmt'] = entry.find(f'{uri}INTERVAL_START_GMT').text
+                report_data['interval'] = entry.find(f'{uri}INTERVAL_NUM').text
+                report_data['value'] = entry.find(f'{uri}VALUE').text
                 reports[resource_name][data_item].append(report_data)
         
             return reports
@@ -214,9 +223,9 @@ class CaisoRequest():
         """ Extracts and parses file. Returns dictionary with data.
         Returns: response = {
             header: {
-                report: "ENE_SLRS",
-                mkt_type: "RTM",
-                UOM: "MW"
+                report: 'ENE_SLRS',
+                mkt_type: 'RTM',
+                UOM: 'MW'
             },
             reports: {
                 0096WD_7_N001: {
@@ -263,9 +272,9 @@ class CaisoRequest():
                     reports[resource_name][data_item] = []
                 
                 report_data = {}
-                report_data["interval_start_gmt"] = entry.find(f'{uri}INTERVAL_START_GMT').text
-                report_data["interval"] = entry.find(f'{uri}INTERVAL_NUM').text
-                report_data["value"] = entry.find(f'{uri}VALUE').text
+                report_data['interval_start_gmt'] = entry.find(f'{uri}INTERVAL_START_GMT').text
+                report_data['interval'] = entry.find(f'{uri}INTERVAL_NUM').text
+                report_data['value'] = entry.find(f'{uri}VALUE').text
                 reports[resource_name][data_item].append(report_data)
         
         return reports
@@ -314,8 +323,8 @@ class CaisoRequest():
 
                 
                 report_data = {}
-                report_data["interval_start_gmt"] = entry.find(f'{uri}INTERVAL_START_GMT').text
-                report_data["value"] = entry.find(f'{uri}VALUE').text
+                report_data['interval_start_gmt'] = entry.find(f'{uri}INTERVAL_START_GMT').text
+                report_data['value'] = entry.find(f'{uri}VALUE').text
                 reports[baa_id][data_item].append(report_data)
         
             return reports
@@ -325,9 +334,7 @@ class CaisoRequest():
         """ Sorts data by interval_num. """
 
         for resource_name in reports:
-            print("RESOURCE NAME HERE", resource_name)
             for data_item in reports[resource_name]: 
-                print("DATA ITEM HERE", data_item)
                 reports[resource_name][data_item].sort(key=self._sort_criteria)
 
         return reports
